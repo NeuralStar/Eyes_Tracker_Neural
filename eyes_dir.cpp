a23fb0b58617d0d6b8f26bb2f78d019ce1bb6270
Chajjar <charles.hajjar.pr@gmail.com>
2023-05-23 19:19:25 +0200

    filtre UVH done

--- eyes_dir.cpp
+++ eyes_dir.cpp
@@ -46,32 +46,46 @@ int main(int argc, const char** argv)
             break;
         }
 
-        vector<int> gazeData = detectAndDisplay(frame);
+        // Convert frame to grayscale
+        Mat frame_gray;
+        cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
 
-	imshow("Eyes Tracker", frame);
+        vector<int> gazeData = detectAndDisplay(frame_gray);
+
+        imshow("Eyes Tracker", frame);
         if (waitKey(1) == 27)
         {
             break; // escape
         }
 
-	cout << "OUTPUT:\n  [";
-	for (auto i = gazeData.begin(); i != gazeData.end(); i++)
-	{
-		cout << *i;
-		if (i + 1 != gazeData.end())
-			cout << ", ";
-	}
-	cout << "]\n";
+        cout << "OUTPUT:\n  [";
+        for (auto i = gazeData.begin(); i != gazeData.end(); i++)
+        {
+            cout << *i;
+            if (i + 1 != gazeData.end())
+                cout << ", ";
+        }
+        cout << "]\n";
     }
 
     return 0;
 }
 
-vector<int> detectAndDisplay(Mat frame)
+
+vector<int> detectAndDisplay(Mat frame_gray)
 {
-    Mat frame_gray;
-    cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
-    equalizeHist(frame_gray, frame_gray);
+    //-- Convert frame_gray to HUV color space
+    Mat frame_hsv;
+    cvtColor(frame_gray, frame_hsv, COLOR_GRAY2BGR);
+    cvtColor(frame_hsv, frame_hsv, COLOR_BGR2HSV);
+
+    //-- Define lower and upper thresholds for dark eye color in HUV space
+    Scalar lower_threshold(0, 0, 0); // Lower threshold for dark eye color
+    Scalar upper_threshold(180, 255, 30); // Upper threshold for dark eye color
+
+    //-- Apply the HUV filter to isolate dark eye color regions
+    Mat filtered_frame;
+    inRange(frame_hsv, lower_threshold, upper_threshold, filtered_frame);
 
     //-- Detect faces
     vector<Rect> faces;
@@ -84,11 +98,11 @@ vector<int> detectAndDisplay(Mat frame)
     for (const auto& face : faces)
     {
         //-- In each face, detect eyes
-        Mat faceROI = frame(face);
-        cvtColor(faceROI, frame_gray, COLOR_BGR2GRAY); // Convert the face ROI to grayscale
+        Mat faceROI = frame_gray(face);
+        Mat filtered_faceROI = filtered_frame(face);
 
         vector<Rect> eyes;
-        eyes_cascade.detectMultiScale(frame_gray, eyes);
+        eyes_cascade.detectMultiScale(faceROI, eyes);
 
         if (eyes.empty()) {
             continue; // No eyes detected, move to the next face
@@ -97,7 +111,9 @@ vector<int> detectAndDisplay(Mat frame)
         for (const auto& eye : eyes)
         {
             Mat eyeROI = faceROI(eye);
-            Point pupil = findPupil(eyeROI);
+            Mat filtered_eyeROI = filtered_faceROI(eye);
+
+            Point pupil = findPupil(filtered_eyeROI);
             Point gazeDirection = Point(eyeROI.cols / 2, eyeROI.rows / 2) - pupil;
 
             int gaze = 0;
@@ -119,36 +135,16 @@ vector<int> detectAndDisplay(Mat frame)
     return {-1, -1, -1}; // Return -1 when no eye detected
 }
 
+
 Point findPupil(Mat eye)
 {
-    Mat gray;
-    cvtColor(eye, gray, COLOR_BGR2GRAY);
-
-    // Blur the image to reduce noise
-    medianBlur(gray, gray, 5);
-
-    // Compute the Scharr gradient
-    Mat grad_x, grad_y;
-    Mat abs_grad_x, abs_grad_y;
-    int scale = 1;
-    int delta = 0;
-    int ddepth = CV_16S;
-
-    // Gradient X
-    Scharr(gray, grad_x, ddepth, 1, 0, scale, delta, BORDER_DEFAULT);
-    convertScaleAbs(grad_x, abs_grad_x);
-
-    // Gradient Y
-    Scharr(gray, grad_y, ddepth, 0, 1, scale, delta, BORDER_DEFAULT);
-    convertScaleAbs(grad_y, abs_grad_y);
-
-    // Total Gradient (approximate)
-    Mat grad;
-    addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad);
-
-    // Threshold and binary the image, black means the pupil region
+    // Apply adaptive thresholding
     Mat bw;
-    adaptiveThreshold(~grad, bw, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 15, -2);
+    adaptiveThreshold(eye, bw, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY_INV, 15, 10);
+
+    // Apply morphological operations for noise removal
+    Mat kernel = getStructuringElement(MORPH_ELLIPSE, Size(5, 5));
+    morphologyEx(bw, bw, MORPH_OPEN, kernel);
 
     // Find the center of gravity
     Moments mu = moments(bw, true);
